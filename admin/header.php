536a12b0bae3d30e3ea7c69af97fc075d8ee766c
M Wildan F <wildanfd11@gmail.com>
2022-10-03 23:43:58 +0700

    Mengubah path tujuan navigation bar

--- admin/header.php
+++ admin/header.php
@@ -27,7 +27,7 @@
     <div class="sticky-top">
         <nav class="navbar navbar-expand-lg navbar-custom navbar-light fw-bold">
             <div class="col-sm-3">
-                <a class="navbar-brand text-white" href="#" style="font-size: 13px; font-weight: bold;"><img class="img-responsive" src="../assets/img/upj_logo.png" alt="Logo UPJ" style="width: 80px; margin-right: 8px;">PMB Universitas Pembangunan Jaya</a>
+                <a class="navbar-brand text-white" href="index.php" style="font-size: 13px; font-weight: bold;"><img class="img-responsive" src="../assets/img/upj_logo.png" alt="Logo UPJ" style="width: 80px; margin-right: 8px;">PMB Universitas Pembangunan Jaya</a>
 
             <!-- Reponsive Hamburger Menu-->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,10 +40,10 @@
                 <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                    <a class="nav-link" href="home.html">Beranda <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="about.html">Jalur Seleksi</a>
+                    <a class="nav-link" href="#jalurSeleksi">Jalur Seleksi</a>
                     </li>
                     <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
